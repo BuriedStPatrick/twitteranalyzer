026a2825f943700f5fe0d9bbf94e2c972871e2ae
BuriedStPatrick <patrick128000@gmail.com>
2015-12-14 21:01:00 +0100

    Updated config template. Separation of responsibility with streamrunner

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 var Twitter = require('twitter');
+var StreamRunner = require('streamrunner');
 var config = require('./config.json');
 var companyList = require("./config.json");
 
 init();
 
-function init(){
+function init() {
 	console.log('runnning');
-}
 	
-function stream(params){
-	client.stream('statuses/filter', {track: '$AAPL'}, function(stream){
+	var client = new Twitter({
+		consumer_key: config.consumer_key,
+		consumer_secret: config.consumer_secret,
+		access_token_key: config.access_token_key,
+		access_token_secret: config.access_token_secret
+	});
+	
+	var stream = new StreamRunner(client,);
+	
+}
+
+function stream(params) {
+	client.stream('statuses/filter', { track: '$AAPL' }, function (stream) {
 		stream.on('data', logTweet);
 		stream.on('error', logError);
 	});
 }
 
-function logTweet(tweet){
+function logTweet(tweet) {
 	console.log(tweet.text);
 }
 
-function logError(error){
+function logError(error) {
 	console.log(error);
 }
 
-function statuses(params){
-	client.get('statuses/user_timeline', params, function(error, tweets, response){
-		if(!error){
+function statuses(params) {
+	client.get('statuses/user_timeline', params, function (error, tweets, response) {
+		if (!error) {
 			console.log(tweets);
-		}else{
+		} else {
 			console.log('error!');
 		}
 	});
 }
 
-var client = new Twitter({
-	consumer_key: config.consumer_key,
-	consumer_secret: config.consumer_secret,
-	access_token_key: config.access_token_key,
-	access_token_secret: config.access_token_secret
-});
+
 
 stream();
--- runners/streamrunner.js
+++ runners/streamrunner.js
@@ -1,7 +1,21 @@
 module.exports = {
-	start : start
+	StreamRunner : StreamRunner
 };
 
-function start(client, companies){
+var StreamRunner = function(client, companies, successFn, errorFn){
+	this.client = client;
+	this.companies = companies;
+	this.successFn = successFn;
+	this.errorFn = errorFn;
 	
+	function run(){
+		client.stream('statuses/filter', { track: '$AAPL' }, function (stream) {
+			stream.on('data', successFn);
+			stream.on('error', errorFn);
+		});
+	}
+	
+	return{
+		run : run
+	}
 }
